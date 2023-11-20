a1c7f0a47ec12bdbedd12e2ffdc2e8ceaa44ec9a
Mai Thành Lợi <83160725+maithanhloi-HT23@users.noreply.github.com>
2023-11-20 10:24:28 +0700

    Update date_default_timezone_set Asia/Ho_Chi_Minh config.php

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 //time zone
-date_default_timezone_set("Asia/Bangkok");
+date_default_timezone_set("Asia/Ho_Chi_Minhk");
 //database connection
 $con=mysqli_connect("localhost","loi_user","Abc@#$234^^^XXD","preschooldb");
 if(mysqli_connect_errno()){
