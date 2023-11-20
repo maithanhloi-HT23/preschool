c4410d345032e509d99441bf3933dcdc00452e25
Mai Thành Lợi <83160725+maithanhloi-HT23@users.noreply.github.com>
2023-11-20 10:39:44 +0700

    Update connect database config.php admin

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,8 +1,8 @@
 <?php
 //time zone
-date_default_timezone_set('Asia/Kolkata');
+date_default_timezone_set("Asia/Ho_Chi_Minhk");
 //database connection
-$con=mysqli_connect("localhost","root","","preschooldb");
+$con=mysqli_connect("localhost","loi_user","Abc@#$234^^^XXD","preschooldb");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
